e1223e21c4d8d4b8302245f5ea6d160f4369a9c9
neojarvis <techadmin@iamneo.ai>
2025-05-24 06:14:35 +0000

    initialCommit-Boilerplate

---
+++ springappwifi/src/main/java/com/examly/springappwifi/SpringappwifiApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappwifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappwifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappwifiApplication.class, args);
+	}
+
+}
