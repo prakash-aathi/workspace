9bbfd268fe2bb6ecdfea4acbda25d178e4e62114
prakash-aathi <prakashaathi321@gmail.com>
2025-05-24 12:28:25 +0530

    Add .gitignore and update Feedback and User entities with ID generation

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/User.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/User.java
@@ -1,12 +1,17 @@
 package com.examly.springappfeedback.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
     // userid, email, password, username, mobileNumber,userRole
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String userId;
     private String email;
     private String password;
