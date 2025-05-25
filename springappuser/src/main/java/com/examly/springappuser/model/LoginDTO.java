6d9320d2f2baab3cf9d7b7614f92c366572a2a2b
prakash-aathi <prakashaathi321@gmail.com>
2025-05-25 19:23:41 +0530

    auth init

--- springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
+++ springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
@@ -1,5 +1,55 @@
 package com.examly.springappuser.model;
 
 public class LoginDTO {
+
+    // token,username,userRole,userId
+
+    private String token;
+    private String username;
+    private String userRole;
+    private String userId;
+
+    public LoginDTO() {
+    }
+
+    public LoginDTO(String token, String username, String userRole, String userId) {
+        this.token = token;
+        this.username = username;
+        this.userRole = userRole;
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
     
 }
--- springappuser/src/main/java/com/examly/springappuser/model/User.java
+++ springappuser/src/main/java/com/examly/springappuser/model/User.java
@@ -1,5 +1,82 @@
 package com.examly.springappuser.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    
+
+    // userid, email, password, username, mobileNumber,userRole
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userId;
+    private String email;
+    private String password;
+    private String username;
+    private String mobileNumber;
+    private String userRole;
+
+    public User() {
+    }
+
+    public User(String userId, String email, String password, String username, String mobileNumber, String userRole) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+        this.userRole = userRole;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
 }
