3eb552079dca445ad59c9ac6e6a68c505c3be59f
neojarvis <techadmin@iamneo.ai>
2025-05-26 14:56:19 +0000

    test commit

--- springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
+++ springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringappserverApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(SpringappserverApplication.class, args);
 	}
 
