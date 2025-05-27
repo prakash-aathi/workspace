e9e3d6b3bd1dd62ebf987deb64b9075b9c57a06b
prakash-aathi <prakashaathi321@gmail.com>
2025-05-27 20:11:07 +0530

    security for particular service

--- springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
+++ springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
@@ -2,6 +2,7 @@ package com.examly.springappuser.config;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -31,10 +32,17 @@ public class JwtUtils {
 
     public String generateJwtToken(Authentication authentication) {
 
+        // set claim for roles
+        HashMap<String, Object> claims = new HashMap<>();
+
         UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
+        if (userPrincipal.getAuthorities() != null) {
+            claims.put("roles", userPrincipal.getAuthorities());
+        }
 
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
+                .setClaims(claims)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key(), SignatureAlgorithm.HS256)
--- springappwifi/src/main/java/com/examly/springappwifi/model/WiFiSchema.java
+++ springappwifi/src/main/java/com/examly/springappwifi/model/WiFiSchema.java
@@ -1,5 +1,100 @@
 package com.examly.springappwifi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class WiFiSchema {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long wifiSchemeId;
+    private String schemeName;
+    private String description;
+    private String region;
+    private String speed;
+    private String dataLimit;
+    private Double fee;
+    private String availabilityStatus;
+
+    public WiFiSchema() {
+    }
+
+    public WiFiSchema(long wifiSchemeId, String schemeName, String description, String speed,
+            String dataLimit, Double fee, String availabilityStatus) {
+        this.wifiSchemeId = wifiSchemeId;
+        this.schemeName = schemeName;
+        this.description = description;
+        this.speed = speed;
+        this.dataLimit = dataLimit;
+        this.fee = fee;
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public long getWifiSchemeId() {
+        return wifiSchemeId;
+    }
+
+    public void setWifiSchemeId(long wifiSchemeId) {
+        this.wifiSchemeId = wifiSchemeId;
+    }
+
+    public String getSchemeName() {
+        return schemeName;
+    }
+
+    public void setSchemeName(String schemeName) {
+        this.schemeName = schemeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getDataLimit() {
+        return dataLimit;
+    }
+
+    public void setDataLimit(String dataLimit) {
+        this.dataLimit = dataLimit;
+    }
+
+    public Double getFee() {
+        return fee;
+    }
+
+    public void setFee(Double fee) {
+        this.fee = fee;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+
 }
