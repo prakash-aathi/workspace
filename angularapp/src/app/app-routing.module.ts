50d3174ea7678a354d1b5446a76208fe79d0d657
prakash-aathi <prakashaathi321@gmail.com>
2025-05-28 22:48:18 +0530

    init login and signup pages

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // default route
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'login' } // fallback route
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ angularapp/src/app/components/adminnav/adminnav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminnav',
+  templateUrl: './adminnav.component.html',
+  styleUrls: ['./adminnav.component.scss']
+})
+export class AdminNavComponent {
+  //   logout() {
+  //   this.authService.logout();
+  //   this.router.navigate(['/login']);
+  // }
+
+}
