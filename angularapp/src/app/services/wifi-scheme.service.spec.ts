c6e63b1633ad618ff073612e451b4f3bbb1691ec
neojarvis <techadmin@iamneo.ai>
2025-05-29 16:46:02 +0000

    Manual-save-commit-5/29/2025--10:16:00-PM

--- angularapp/src/app/services/wifi-scheme.service.spec.ts
+++ angularapp/src/app/services/wifi-scheme.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WifiSchemeService } from './wifi-scheme.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WifiSchemeService', () => {
   let service: WifiSchemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(WifiSchemeService);
   });
 
-  it('should be created', () => {
+  fit('Frontend_should_create_wifischeme_service', () => {
     expect(service).toBeTruthy();
   });
 });
